fc60c22a2513c410168919aebac0ab70c81077fc
shwethadia <shwethash11@gmail.com>
2022-05-16 14:42:46 +0530

    Added Header front end

--- next.config.js
+++ next.config.js
@@ -2,3 +2,9 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+module.exports = {
+  images: {
+    domains: ['s2.coinmarketcap.com', 's3.coinmarketcap.com', 'cdn.sanity.io', 'encrypted-tbn0.gstatic.com']
+  },
+}
